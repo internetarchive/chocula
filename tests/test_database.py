cd03ba1cd1c3e05a16ef2ed326f9847f411ee53d
Bryan Newbold <bnewbold@archive.org>
2020-05-07 19:56:51 -0700

    start of basic tests

---
+++ tests/test_database.py
@@ -0,0 +1,15 @@
+
+from chocula.database import IssnDatabase
+
+
+def test_issn_database():
+
+    issn_db = IssnDatabase(issn_issnl_file_path="tests/files/ISSN-to-ISSN-L.txt")
+
+    assert issn_db.issn2issnl('1234-5678') is None
+    assert issn_db.issn2issnl('0000-0000') is None
+
+    # "The Lancet"
+    assert issn_db.issn2issnl('0140-6736') == '0140-6736'
+    assert issn_db.issn2issnl('1474-547X') == '0140-6736'
+
---
+++ tests/test_directories.py
@@ -0,0 +1,29 @@
+
+import pytest
+from chocula import *
+
+@pytest.fixture
+def config():
+    config = ChoculaConfig.from_file(sources_dir="tests/files/")
+    return config
+
+@pytest.fixture
+def issn_db():
+    return IssnDatabase(issn_issnl_file_path="tests/files/ISSN-to-ISSN-L.txt")
+
+@pytest.fixture
+def database(issn_db):
+    db = ChoculaDatabase(db_file=":memory:", issn_db=issn_db)
+    db.init_db()
+    return db
+
+def test_all(config, database):
+
+    for cls in (CrossrefLoader, DoajLoader, EntrezLoader, EzbLoader,
+                GoldOALoader, NorwegianLoader, OpenAPCLoader, RoadLoader,
+                SzczepanskiLoader, WikidataLoader):
+        # XXX: SherpaRomeoLoader
+        loader = cls(config)
+        counts = loader.index_file(database)
+        assert counts['total'] >= 20
+        assert counts['inserted'] > 5
